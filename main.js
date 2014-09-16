9a7ed4dbe01220f348efc541d3b6a9fbaf7e2798
NoahWilson4 <noahrwilson4@gmail.com>
2014-09-16 12:32:32 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,144 @@
+// 1
+
+// function tripleFive(x) {
+
+// 	for (i = 1; i < 4; i++){
+// 		console.log(x);
+// 	}
+// }
+
+// var word = 'Triple Five!';
+
+// tripleFive(word);
+
+
+
+// 2
+
+// function lastLetter(x) {
+// 	var letter = x[x.length - 1];
+// 	console.log(letter);
+// }
+
+// var word = 'hello';
+// lastLetter(word);
+
+// 3
+
+// function square(x) {
+// 	x = x * x;
+// 	console.log(x);
+// 	return x;
+// }
+
+// square(3);
+
+// 4
+
+// function negate(x){
+// 	x = x - (x * 2);
+// 	console.log(x);
+// 	return x;
+// }
+
+// negate(5);
+
+
+// 5
+
+// function toArray(a, b, c){
+// 	var array = [];
+// 	array.push(a);
+// 	array.push(b);
+// 	array.push(c);
+// 	console.log(array)
+// 	return array;
+// }
+
+// toArray(1, 4, 5);
+
+// 6
+
+// function startsWithA(x) {
+// 	x = x[0];
+// 	console.log(x);
+// 	if (x = 'a'){
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+	
+// };
+
+// var isA = startsWithA('apple');
+// console.log(isA);
+
+
+//  7
+
+// function excite(x){
+// 	x = x + '!!!';
+// 	console.log(x);
+// 	return x;
+// }
+
+// excite('hello');
+
+// 8
+
+// function sun(x) {
+// 	var y = x.indexOf('sun');
+// 	if (x[y] === 's' && x[y + 1] === 'u' && x[y + 2] === 'n') {
+// 			return true;
+// 	} else {return false;}
+// }
+
+// var word = sun('sundried');
+// console.log(word);
+
+
+// 9
+
+// function tiny(x) {
+// 	if (x < 1 && x > 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+
+// }
+
+// var isSmall = tiny(.5);
+	// console.log(isSmall);
+
+//  10
+
+// function getSeconds (x) {
+// 	x = x.split(':');
+// 	console.log(x);
+// 	var a = Number(x[0]);
+// 	var b = Number(x[1]);
+// 	x = (a * 60) + b;
+// 	console.log(x);
+// 	return x;
+
+// }
+
+
+// getSeconds('10:25');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
